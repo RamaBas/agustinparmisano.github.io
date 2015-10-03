32b5c88288b7ebb4e66f7a94e04c2203dd4d6106
agustin <agustin@debian8.agustin-debian.edu>
2015-10-02 23:49:00 -0300

    Cambiando el datepicker por jsdatepick y cambiando la forma en deslizar las pantallas a todas horizontales por separado

--- freeDomInterface/js/charts/stats.js
+++ freeDomInterface/js/charts/stats.js
@@ -64,33 +64,12 @@ $(document).ready(function(){
                 data: [20, 20, 20, 30, 40, 50, 40, 40, 50, 60, 55, 65, 70, 85, 80, 80, 85, 90, 70, 50, 40, 20, 20, 20],
                 pointInterval: 3600 * 1000, // one day]
                 color: "#d32f2f",
+                type: 'line',
             }           
             ]
         });
     });
 
-    $('#example1').datepicker({
-        format: "dd/mm/yyyy",
-        keyboardNavigation: true
-    }).on('changeDate', function(ev){
-
-        var arr = [];
-        while(arr.length < 24){
-            var randomnumber=Math.ceil(Math.random()*100)
-            var found=false;
-            for(var i=0;i<arr.length;i++){
-                if(arr[i]==randomnumber){found=true;break}
-            }
-            if(!found)arr[arr.length]=randomnumber;
-        }
-
-        $('#temp-container').highcharts().addSeries({
-            name: "Temperatura en " + (ev.date.getDate() + '/' + (ev.date.getMonth() + 1)  + '/' +  ev.date.getFullYear()),
-            data: arr,//[10, 10, 50, 70, 30, 20, 10, 70, 90, 60, 35, 65, 70, 85, 80, 80, 85, 90, 70, 50, 40, 20, 20, 20],
-            pointInterval: 3600 * 1000, // one day]
-            color: "#d32f2f"
-        });
-    });
 
     $(function () {
         $('#hume-container').highcharts({
@@ -163,29 +142,6 @@ $(document).ready(function(){
         });
     });
 
-    $('#example2').datepicker({
-        format: "dd/mm/yyyy",
-        keyboardNavigation: true
-    }).on('changeDate', function(ev){
-
-        var arr = [];
-        while(arr.length < 24){
-            var randomnumber=Math.ceil(Math.random()*100)
-            var found=false;
-            for(var i=0;i<arr.length;i++){
-                if(arr[i]==randomnumber){found=true;break}
-            }
-            if(!found)arr[arr.length]=randomnumber;
-        }
-
-        $('#hume-container').highcharts().addSeries({
-            name: "Humedad en " + (ev.date.getDate() + '/' + (ev.date.getMonth() + 1)  + '/' +  ev.date.getFullYear()),
-            data: arr,//[10, 10, 50, 70, 30, 20, 10, 70, 90, 60, 35, 65, 70, 85, 80, 80, 85, 90, 70, 50, 40, 20, 20, 20],
-            pointInterval: 3600 * 1000, // one day]
-            color: "#1976d2"
-        });
-
-    });
 
 
     $(function () {
@@ -252,34 +208,126 @@ $(document).ready(function(){
                 name: 'Electricidad Consumida Hoy',
                 data: [40, 30, 10, 20, 30, 40, 50, 60, 70, 60, 50, 40, 30, 40, 50, 60, 50, 40, 30, 20, 10, 00, 10, 20],
                 pointInterval: 3600 * 1000, // one day]
-                color: "#388e3c"
+                color: "#388e3c",
             }            
             ]
         });
     });
 
-    $('#example3').datepicker({
-        format: "dd/mm/yyyy",
-        keyboardNavigation: true
-    }).on('changeDate', function(ev){
 
-        var arr = [];
-        while(arr.length < 24){
-            var randomnumber=Math.ceil(Math.random()*100)
-            var found=false;
-            for(var i=0;i<arr.length;i++){
-                if(arr[i]==randomnumber){found=true;break}
+    window.onload = function(){
+        
+        
+        g_globalObjectTemp = new JsDatePick({
+            useMode:1,
+            isStripped:true,
+            target:"temp"
+            /*selectedDate:{                This is an example of what the full configuration offers.
+                day:5,                      For full documentation about these settings please see the full version of the code.
+                month:9,
+                year:2006
+            },
+            yearsRange:[1978,2020],
+            limitToToday:false,
+            cellColorScheme:"beige",
+            dateFormat:"%m-%d-%Y",
+            imgPath:"img/",
+            weekStartDay:1*/
+        });     
+        
+        g_globalObjectTemp.setOnSelectedDelegate(function(){
+            var arr = [];
+            while(arr.length < 24){
+                var randomnumber=Math.ceil(Math.random()*100)
+                var found=false;
+                for(var i=0;i<arr.length;i++){
+                    if(arr[i]==randomnumber){found=true;break}
+                }
+                if(!found)arr[arr.length]=randomnumber;
             }
-            if(!found)arr[arr.length]=randomnumber;
-        }
+            var objTemp = g_globalObjectTemp.getSelectedDay();
+            $('#temp-container').highcharts().addSeries({
+                name: "Temperatura en " + (objTemp.day + '/' + objTemp.month  + '/' +  objTemp.year),
+                data: arr,//[10, 10, 50, 70, 30, 20, 10, 70, 90, 60, 35, 65, 70, 85, 80, 80, 85, 90, 70, 50, 40, 20, 20, 20],
+                pointInterval: 3600 * 1000, // one day]
+                color: "#d32f2f"
+            });
 
-        $('#electric-container').highcharts().addSeries({
-            name: "Electricidad consumida el " + (ev.date.getDate() + '/' + (ev.date.getMonth() + 1)  + '/' +  ev.date.getFullYear()),
-            data: arr,//[10, 10, 50, 70, 30, 20, 10, 70, 90, 60, 35, 65, 70, 85, 80, 80, 85, 90, 70, 50, 40, 20, 20, 20],
-            pointInterval: 3600 * 1000, // one day]
-            color: "#388e3c"
         });
 
-    });
+        g_globalObjectHume = new JsDatePick({
+            useMode:1,
+            isStripped:true,
+            target:"hume"
+            /*selectedDate:{                This is an example of what the full configuration offers.
+                day:5,                      For full documentation about these settings please see the full version of the code.
+                month:9,
+                year:2006
+            },
+            yearsRange:[1978,2020],
+            limitToToday:false,
+            cellColorScheme:"beige",
+            dateFormat:"%m-%d-%Y",
+            imgPath:"img/",
+            weekStartDay:1*/
+        });     
+        
+        g_globalObjectHume.setOnSelectedDelegate(function(){
+            var objHume = g_globalObjectHume.getSelectedDay();
+            var arr = [];
+            while(arr.length < 24){
+                var randomnumber=Math.ceil(Math.random()*100)
+                var found=false;
+                for(var i=0;i<arr.length;i++){
+                    if(arr[i]==randomnumber){found=true;break}
+                }
+                if(!found)arr[arr.length]=randomnumber;
+            }
 
+            $('#hume-container').highcharts().addSeries({
+                name: "Humedad en " + (objHume.day + '/' + objHume.month  + '/' +  objHume.year),
+                data: arr,//[10, 10, 50, 70, 30, 20, 10, 70, 90, 60, 35, 65, 70, 85, 80, 80, 85, 90, 70, 50, 40, 20, 20, 20],
+                pointInterval: 3600 * 1000, // one day]
+                color: "#1976d2"
+            });
+        });
+
+        g_globalObjectElect = new JsDatePick({
+            useMode:1,
+            isStripped:true,
+            target:"elect"
+            /*selectedDate:{                This is an example of what the full configuration offers.
+                day:5,                      For full documentation about these settings please see the full version of the code.
+                month:9,
+                year:2006
+            },
+            yearsRange:[1978,2020],
+            limitToToday:false,
+            cellColorScheme:"beige",
+            dateFormat:"%m-%d-%Y",
+            imgPath:"img/",
+            weekStartDay:1*/
+        });     
+        
+        g_globalObjectElect.setOnSelectedDelegate(function(){
+            var objElect = g_globalObjectElect.getSelectedDay();
+            var arr = [];
+            while(arr.length < 24){
+                var randomnumber=Math.ceil(Math.random()*100)
+                var found=false;
+                for(var i=0;i<arr.length;i++){
+                    if(arr[i]==randomnumber){found=true;break}
+                }
+                if(!found)arr[arr.length]=randomnumber;
+            }
+
+            $('#electric-container').highcharts().addSeries({
+                name: "Electricidad consumida el " + (objElect.day + '/' + objElect.month  + '/' +  objElect.year),
+                data: arr,//[10, 10, 50, 70, 30, 20, 10, 70, 90, 60, 35, 65, 70, 85, 80, 80, 85, 90, 70, 50, 40, 20, 20, 20],
+                pointInterval: 3600 * 1000, // one day]
+                color: "#388e3c",
+            });            
+        });
+    };
 });
+
